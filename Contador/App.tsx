3317dec0ab1a7fc58fd8605777ad0f84f070e085
GermanPublyland <german.estrade@hotmail.com>
2021-06-28 11:23:16 +0200

    Fin calculadora

--- Contador/App.tsx
+++ Contador/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 import { ContadorScreen } from './src/screens/ContadorScreen';
 
 export const App = () => {
 
   return (
-    // <HolaMundoScreen />
-    <ContadorScreen />
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* // <HolaMundoScreen /> */}
+      <ContadorScreen />
+      
+    </SafeAreaView>
   )
 }
